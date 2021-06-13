ddb9574f594cef770a234310b6d60668f3b2c7a8
Mohit <mohitsingh.it5@gmail.com>
2021-06-13 22:07:14 +0530

    Added Zoomed,filter capture, Improved dir structure | Mohit

--- CameraApp/script.js
+++ CameraApp/script.js
@@ -3,117 +3,152 @@ let recBtn = document.querySelector("#recording");
 let recDiv = recBtn.querySelector("div");
 let capBtn = document.querySelector("#capture");
 let capDiv = capBtn.querySelector("div");
-let body= document.querySelector("body");
+let body = document.querySelector("body");
 let isRecording = false;
 let chunks = [];
 let mediaRecorder;
 let appliedFilter;
-let filters= document.querySelectorAll(".filter");
+let filters = document.querySelectorAll(".filter");
+let currentZoom = 1;
+const minZoom = 1;
+const maxZoom = 3;
 
-      for(let i=0;i<filters.length;i++){
-        filters[i].addEventListener("click",function(e){
-          removeFilters();
-          appliedFilter = e.currentTarget.style.backgroundColor;
-          let div = document.createElement("div");
-          div.style.backgroundColor=appliedFilter;
-          div.classList.add("filter-div");
-          body.append(div);
-        });
-      }
+//zoom functionalities
 
-      //single btn
-      recBtn.addEventListener("click", function (e) {
-        if (isRecording == false) {
-          isRecording = true;
-          mediaRecorder.start();
-          recDiv.classList.add("recording-animation");
-        } else {
-          isRecording = false;
-          mediaRecorder.stop();
-          recDiv.classList.remove("recording-animation");
-        }
-      });
+let zoomInBtn = document.querySelector(".zoom-in");
+let zoomOutBtn = document.querySelector(".zoom-out");
 
-      capBtn.addEventListener("click", function (e) {
-        let canvas = document.createElement("canvas");
-        capDiv.classList.add("capture-animation");
-        setTimeout(function (e) {
-          capDiv.classList.remove("capture-animation");
-        }, 1000);
-        // get the current camera'sresolution
-        canvas.height = video.videoHeight;
-        canvas.width = video.videoWidth;
-        let tool = canvas.getContext("2d");
-        tool.drawImage(video, 0, 0);
-        // data url directly contains the data(does'nt points to a resource) so no need of blob
-        let link = canvas.toDataURL();
-        let a = document.createElement("a");
-        a.href = link;
-        a.download = "image.jpg";
-        a.click();
-        a.remove();
-        canvas.remove();
-      });
+zoomInBtn.addEventListener("click", function (e) {
+  if (currentZoom < maxZoom) {
+    currentZoom = currentZoom + 0.1;
+  }
+  video.style.transform = `scale(${currentZoom})`;
+});
+zoomOutBtn.addEventListener("click", function (e) {
+  if (currentZoom > minZoom) {
+    currentZoom = currentZoom - 0.1;
+  }
+  video.style.transform = `scale(${currentZoom})`;
+});
 
-      // navigator.mediaDevices.getUserMedia takes object of devices required and is a promise
-      // asks a single permission for both audio and video
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: true })
-        .then(function (mediaStream) {
-          //resolving promise
+//filters
 
-          /*
+function removeFilters() {
+  let Filter = document.querySelector(".filter-div");
+  if (Filter) Filter.remove();
+}
+
+
+for (let i = 0; i < filters.length; i++) {
+  filters[i].addEventListener("click", function (e) {
+    removeFilters();
+    appliedFilter = e.currentTarget.style.backgroundColor;
+    let div = document.createElement("div");
+    div.style.backgroundColor = appliedFilter;
+    div.classList.add("filter-div");
+    body.append(div);
+  });
+}
+
+
+//recording
+recBtn.addEventListener("click", function (e) {
+  if (isRecording == false) {
+    isRecording = true;
+    mediaRecorder.start();
+    appliedFilter = ""; //color remove -> no recording with filters
+    removeFilter(); //remove from ui too
+
+    //zooming not allowed in vide recording
+    currentZoom=1;
+    video.style.transform="scale(1)";
+    recDiv.classList.add("recording-animation");
+  } else {
+    isRecording = false;
+    mediaRecorder.stop();
+    recDiv.classList.remove("recording-animation");
+  }
+});
+
+
+//image capture
+capBtn.addEventListener("click", function (e) {
+  let canvas = document.createElement("canvas");
+  capDiv.classList.add("capture-animation");
+
+  //removing animation after 1sec for next capture addition
+  setTimeout(function (e) {
+    capDiv.classList.remove("capture-animation");
+  }, 1000);
+  // get the current camera'sresolution
+  canvas.height = video.videoHeight;
+  canvas.width = video.videoWidth;
+  let tool = canvas.getContext("2d");
+
+  //shifting origin
+  let X = canvas.width/2;
+  let Y = canvas.height/2;
+  tool.translate(X,Y);
+
+  tool.scale(currentZoom,currentZoom);
+  //reshiting back to top-left
+  tool.translate(-X,-Y);
+
+  tool.drawImage(video, 0, 0);
+  if (appliedFilter) {
+    tool.fillStyle = appliedFilter;
+    tool.fillRect(0, 0, canvas.width, canvas.height);
+  }
+  let link = canvas.toDataURL();
+  let a = document.createElement("a");
+  a.href = link;
+  a.download = "image.jpg";
+  a.click();
+  a.remove();
+  canvas.remove();
+});
+
+
+navigator.mediaDevices
+  .getUserMedia({ video: true, audio: true })
+  .then(function (mediaStream) {
+    //resolving promise
+
+    /*
             //in case of blank video/codec problem
             let option = {mimeType: 'video/webm'};
             mediaRecorder = new MediaRecorder(mediaStream,option);
             */
-          mediaRecorder = new MediaRecorder(mediaStream);
+    mediaRecorder = new MediaRecorder(mediaStream);
 
-          //dataavailabe
-          mediaRecorder.addEventListener("dataavailable", function (e) {
-            chunks.push(e.data);
-          });
+    //dataavailabe
+    mediaRecorder.addEventListener("dataavailable", function (e) {
+      chunks.push(e.data);
+    });
 
-          //stop
-          mediaRecorder.addEventListener("stop", function (e) {
-            let blob = new Blob(chunks, { type: "video/mp4" });
+    //stop
+    mediaRecorder.addEventListener("stop", function (e) {
+      let blob = new Blob(chunks, { type: "video/mp4" });
 
-            //in case of option is provided to mediaRecorder
-            // let blob = new Blob(chunks, {type: "video/mp4"});
+      //in case of option is provided to mediaRecorder
+      // let blob = new Blob(chunks, {type: "video/mp4"});
 
-            chunks = []; //clearing for further recordings
+      chunks = []; //clearing for further recordings
+      let a = document.createElement("a");
+      let url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = "video.mp4";
+      a.click();
+      a.remove();
+    });
 
-            // creating anchor
-            let a = document.createElement("a");
+    // video.style.overflow = "hidden";
+    video.srcObject = mediaStream;
+  })
+  .catch(function (err) {
+    // when promise is rejected
+    console.log(err); //Permission Denied.
+  });
 
-            //creating URL and attaching that to anchor
-            let url = window.URL.createObjectURL(blob);
-            a.href = url;
+//  we can make seperate permission for audio and video => seperate objects and the we have to  sync audio and video
 
-            //Naming the file to be downloaded
-            a.download = "video.mp4";
-            // a.download= "video.webm"
-
-            //clicking the anchor
-            a.click();
-            //no need or anchor now
-            a.remove();
-          });
-          // video.height = screen.height;
-          video.width = screen.width;
-          video.style.overflow = "hidden";
-          video.srcObject = mediaStream;
-        })
-        .catch(function (err) {
-          // when promise is rejected
-          console.log(err); //Permission Denied.
-        });
-
-      //  we can make seperate permission for audio and video => seperate objects and the we have to  sync audio and video
-
-
-
-function removeFilters(){
-  let Filter = document.querySelector(".filter-div");
-  if(Filter) Filter.remove();
-}
