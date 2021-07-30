137fd02f6e79f9e7b4d7a1f49bf2312dd87e9bd3
Mohit <mohitsingh.it5@gmail.com>
2021-07-30 19:44:30 +0530

    realtime/event-based data done | Mohit

--- anon-twitter/src/App.js
+++ anon-twitter/src/App.js
@@ -3,25 +3,24 @@ import "./App.css";
 //importing {} because firestore wasn't the default import
 import { firestore } from "./firebase";
 function App() {
-  let [posts, setData] = useState([]);
-  useEffect(() => {
-    // as promise is returned
-    let fx = async () => {
-      //get the refrece of main collection
-      let ref = firestore.collection("posts");
-      let querySnapshot = await ref.get(); //actual cusror to documents in collection/querySnapshot
-      // collection have it's own forEach method for iteration
-      let tmp = [];
-      querySnapshot.forEach((doc) => {
-        tmp.push(doc);
-      });
+  let [posts, setPosts] = useState([]);
 
-      setData(tmp);
+  useEffect(() => {
+    let f = async () => {
+      await firestore.collection("posts").onSnapshot((querySnapshot) => {
+        let tempArr = [];
+
+        querySnapshot.forEach((doc) => {
+          tempArr.push(doc);
+        });
+
+        setPosts(tempArr);
+      });
     };
 
-    //calling that fx
-    fx();
+    f();
   }, []);
+
   return (
     <div>
       <ul>
@@ -30,7 +29,7 @@ function App() {
         })}
       </ul>
       <input
-        placeholder="What's on yuor Mind?"
+        placeholder="What's on your Mind?"
         onKeyDown={(e) => {
           let val = e.currentTarget.value.trim();
           if (val !== "" && e.key === "Enter") {
