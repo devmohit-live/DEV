94879455a81cccdd1f24a3a3fd80b6b0f6541e1b
Mohit <mohitsingh.it5@gmail.com>
2021-07-31 22:34:43 +0530

    update

--- anon-twitter/src/firebase.js
+++ anon-twitter/src/firebase.js
@@ -1,19 +1,11 @@
 //istead of importing all the firebase we nned only the app component of it
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 //getting the firestore from that firebase(imported)
-import 'firebase/firestore'
+import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA_938sgeKExijmkVUfJs6rjiF9IVEEQo8",
-  authDomain: "anontwitter-fcb9c.firebaseapp.com",
-  projectId: "anontwitter-fcb9c",
-  storageBucket: "anontwitter-fcb9c.appspot.com",
-  messagingSenderId: "437070415571",
-  appId: "1:437070415571:web:44dfd8ab6645c2e9317626",
-};
+const firebaseConfig = require("./conf.json");
 // Initialize Firebase with given config
 firebase.initializeApp(firebaseConfig);
-
 
 //exporting firestore
 export const firestore = firebase.firestore();
